7c68fdcebe29bbe704cae886c32aae734f28ed89
stalinchidhamparam <stalinchidhamparam@gmail.com>
2025-12-10 11:45:06 +0530

    Remove unnecessary blank line in App.jsx
    Deleted an extra blank line after imports in App.jsx to improve code readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React, { useEffect, useState, Suspense, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import ScrollToTop from './components/ScrollTop';
 import Footer from './components/Footer';
